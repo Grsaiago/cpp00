5b5ffdcb59be4e2bcd034cf96487620671cf156f
Gabriel Saiago <gsaiago@student.42.rio>
2024-04-09 20:37:01 -0300

    07-01 Add: include Array.hpp in iter.hpp

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,26 +1,22 @@
 #ifndef ITER_HPP
-# define ITER_HPP
+#define ITER_HPP
+#include "Array.hpp"
 
-template<typename T>
-void	dup(T &elem)
-{
-	elem += elem;
-	return ;
+template <typename T> void dup(T &elem) {
+  elem += elem;
+  return;
 }
 
-template<typename T>
-void	add(T &elem)
-{
-	elem += 1;
-	return ;
+template <typename T> void add(T &elem) {
+  elem += 1;
+  return;
 }
 
-template<typename T>
-void	iter(Array<T> &arr, int len, void(*f)(T &elem)) {
-	for (int i = 0; i < len; i++) {
-		f(arr[i]);
-	}
-	return ;
+template <typename T> void iter(Array<T> &arr, int len, void (*f)(T &elem)) {
+  for (int i = 0; i < len; i++) {
+    f(arr[i]);
+  }
+  return;
 }
 
 #endif // !ITER_HPP
