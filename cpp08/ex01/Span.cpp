6f027e6fb3715371df9cc5b8d08e2f4582d2b143
Gabriel Saiago <gsaiago@student.42.rio>
2024-04-11 17:53:58 -0300

    07-01: Span fill method

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,87 +1,81 @@
 #include "Span.hpp"
+#include <cstdlib>
+#include <exception>
 
-Span::Span(unsigned int capacity) : _capacity(capacity), _size(0)
-{ }
+Span::Span(unsigned int capacity) : _capacity(capacity), _size(0) {}
 
-Span::Span(Span const &cpy) : _internal_lst(cpy.getLst()), _capacity(cpy.getCapacity()), _size(0)
-{ }
+Span::Span(Span const &cpy)
+    : _capacity(cpy.getCapacity()), _size(0), _internal_lst(cpy.getLst()) {}
 
-Span::~Span(void)
-{ }
+Span::~Span(void) {}
 
-Span	&Span::operator=(Span const &rhs)
-{
-	this->_internal_lst = rhs.getLst();
-	this->_capacity = rhs.getCapacity();
-	return (*this);
+Span &Span::operator=(Span const &rhs) {
+  this->_internal_lst = rhs.getLst();
+  this->_capacity = rhs.getCapacity();
+  return (*this);
 }
 
-unsigned int	Span::getCapacity(void) const
-{
-	return (this->_capacity);
+unsigned int Span::getCapacity(void) const { return (this->_capacity); }
+
+unsigned int Span::getSize(void) const { return (this->_size); }
+
+std::list<int> Span::getLst(void) const {
+  return (std::list<int>(this->_internal_lst));
+}
+void Span::addNumber(unsigned int numb) {
+  if (this->_size == this->_capacity)
+    throw std::length_error("container full");
+  this->_internal_lst.push_back(numb);
+  this->_size++;
 }
 
-unsigned int	Span::getSize(void) const
-{
-	return (this->_size);
+unsigned int Span::longestSpan(void) const {
+  if (this->_size < 2)
+    throw std::length_error("not enough items");
+  return (*std::max_element(this->_internal_lst.begin(),
+                            this->_internal_lst.end()) -
+          *std::min_element(this->_internal_lst.begin(),
+                            this->_internal_lst.end()));
 }
 
-std::list<int>	Span::getLst(void) const
-{
-	return (std::list<int>(this->_internal_lst));
-}
-void	Span::addNumber(unsigned int numb)
-{
-	if (this->_size == this->_capacity)
-		throw std::length_error("container full");
-	this->_internal_lst.push_back(numb);
-	this->_size++;
-}
-
-unsigned int	Span::longestSpan(void) const
-{
-	if (this->_size < 2)
-		throw std::length_error("not enough items");
-	return ( *std::max_element(this->_internal_lst.begin(), this->_internal_lst.end())
-		- *std::min_element(this->_internal_lst.begin(), this->_internal_lst.end()) );
+unsigned int Span::shortestSpan(void) const {
+  int min = this->longestSpan();
+  if (this->_size < 2)
+    throw std::length_error("not enough items");
+  for (std::list<int>::const_iterator i = this->_internal_lst.begin();
+       i != this->_internal_lst.end(); i++) {
+    for (std::list<int>::const_iterator j = this->_internal_lst.begin();
+         j != this->_internal_lst.end(); j++) {
+      if (i == j)
+        continue;
+      if (std::abs(*i - *j) < min)
+        min = std::abs(*i - *j);
+    }
+  }
+  return (min);
 }
 
-unsigned int	Span::shortestSpan(void) const
-{
-	int min = this->longestSpan();
-	if (this->_size < 2)
-		throw std::length_error("not enough items");
-	for (std::list<int>::const_iterator i = this->_internal_lst.begin(); i != this->_internal_lst.end(); i++)
-	{
-		for (std::list<int>::const_iterator j = this->_internal_lst.begin(); j != this->_internal_lst.end(); j++)
-		{
-			if (i == j)
-				continue;
-			if (std::abs(*i - *j) < min)
-				min = std::abs(*i - *j);
-		}
-	}
-	return (min);
+void Span::fillSpan(void) {
+  try {
+    while (true) {
+      this->addNumber(rand());
+    }
+  } catch (std::exception &err) {
+    ;
+  }
+  return;
 }
 
-void	Span::fillSpan(void)
-{
-	for (std::list<int>::const_iterator i = this->_internal_lst.begin(); i != this->_internal_lst.end(); i++)
-	{
-	}
+void Span::putSpan(std::ostream &os) const {
+  for (std::list<int>::const_iterator i = this->_internal_lst.begin();
+       i != this->_internal_lst.end(); i++) {
+    os << *i << ' ';
+  }
+  return;
 }
 
-void	Span::putSpan(std::ostream &os) const
-{
-	for (std::list<int>::const_iterator i = this->_internal_lst.begin(); i != this->_internal_lst.end(); i++) {
-		os << *i << ' ';
-	}
-	return ;
-}
-
-std::ostream	&operator<<(std::ostream &os, Span const &rhs)
-{
-	rhs.putSpan(os);
-	os << std::endl;
-	return (os);
+std::ostream &operator<<(std::ostream &os, Span const &rhs) {
+  rhs.putSpan(os);
+  os << std::endl;
+  return (os);
 }
--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -1,34 +1,34 @@
 #ifndef SPAN_HPP
-# define SPAN_HPP
-# include <list>
-# include <stdexcept>
-# include <algorithm>
-# include <iostream>
+#define SPAN_HPP
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <stdexcept>
 
-class Span
-{
-	public:
-	// constructors/destructors
-		Span(unsigned int capacity);
-		Span(Span const &cpy);
-		~Span(void);
-	// operator overloads
-		Span&	operator=(Span const &rhs);
-	// methods
-		unsigned int	getCapacity(void) const;
-		unsigned int	getSize(void) const;
-		std::list<int>	getLst(void) const;
-		void		addNumber(unsigned int numb);
-		unsigned int	longestSpan(void) const;
-		unsigned int	shortestSpan(void) const;
-		void		fillSpan(void);
-		void		putSpan(std::ostream &os) const;
-	private:
-		unsigned int		_capacity;
-		unsigned int		_size;
-		std::list<int>		_internal_lst;
+class Span {
+public:
+  // constructors/destructors
+  Span(unsigned int capacity);
+  Span(Span const &cpy);
+  ~Span(void);
+  // operator overloads
+  Span &operator=(Span const &rhs);
+  // methods
+  unsigned int getCapacity(void) const;
+  unsigned int getSize(void) const;
+  std::list<int> getLst(void) const;
+  void addNumber(unsigned int numb);
+  unsigned int longestSpan(void) const;
+  unsigned int shortestSpan(void) const;
+  void fillSpan(void);
+  void putSpan(std::ostream &os) const;
+
+private:
+  unsigned int _capacity;
+  unsigned int _size;
+  std::list<int> _internal_lst;
 };
 
-std::ostream	&operator<<(std::ostream &os, Span const &rhs);
+std::ostream &operator<<(std::ostream &os, Span const &rhs);
 
 #endif
